ad99e47b7a19c28640970ab2f548815367434873
victmartgarc-cyber <victmartgarc@gmail.com>
2026-07-02 23:44:39 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE = "vocab-trainer-v1";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon.png"
+];
+
+self.addEventListener("install", e=>{
+    e.waitUntil(
+        caches.open(CACHE).then(cache=>cache.addAll(FILES))
+    );
+});
+
+self.addEventListener("fetch", e=>{
+    e.respondWith(
+        caches.match(e.request).then(r=>r || fetch(e.request))
+    );
+});
